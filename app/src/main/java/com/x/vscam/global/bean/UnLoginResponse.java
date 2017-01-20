5475c8d783efaec994b899ae09fb18e80ea16382
ayaseruri <ayaseruri@foxmail.com>
2017-01-20 17:08:21 +0800

    1、预发布

---
+++ app/src/main/java/com/x/vscam/global/bean/UnLoginResponse.java
@@ -0,0 +1,21 @@
+package com.x.vscam.global.bean;
+
+/**
+ * Created by wufeiyang on 2017/1/20.
+ */
+
+public class UnLoginResponse {
+    /**
+     * error : 尚未登录
+     */
+
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+## VSCAM(Android)
+### 手机拍摄、胶片味、意识流
+#### 一个简单的分享VSCO调整修改过的照片的平台
+#### 同样也有IOS客户端 [IOS Github](https://eyrefree.github.io/VSCAM/)
+##### 特点：
+  * Material Design
+  * 快速、极简
+
+##### 截图：
+
+##### 下载：
+[Android GooglePlay]()
